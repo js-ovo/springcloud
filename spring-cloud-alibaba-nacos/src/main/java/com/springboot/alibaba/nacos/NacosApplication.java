bb9daa937cf2b2f2e72caf93c78760cd14981ceb
TuT <jing_991222@163.com>
2024-03-08 19:57:20 +0800

    spring cloud study project init!

---
+++ spring-cloud-alibaba-nacos/src/main/java/com/springboot/alibaba/nacos/NacosApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.alibaba.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class,args);
+    }
+}
---
+++ spring-cloud-consul/src/main/java/com/springboot/consul/ConsulApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulApplication.class,args);
+    }
+}
